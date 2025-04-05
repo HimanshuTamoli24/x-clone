8723cddf0ca30c7d4e93bc79d12c18157d627af9
Himanshu Tamoli <himanshutamoli2005@gmail.com>
2025-04-05 18:16:01 +0530

    Removed backend from frontend

--- Backend/src/Utils/ApiErrors.js
+++
@@ -1,8 +0,0 @@
-class ApiErrors {
-    constructor(statusCode, message = "unsuccessful", error = []) {
-        this.statusCode = statusCode;
-        this.message = message;
-        this.error = error;
-    }
-}
-export default ApiErrors;
